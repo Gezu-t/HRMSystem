eaf52c96476a0bb0437ab0b3a38c1bd5cdb78db5
GezahegnTsegaye <gezut38@gmail.com>
2023-01-15 15:53:52 +0100

    modification on the hrms client

---
+++ hrms-client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { EducationFormComponent } from './education-form/education-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeFormComponent,
+    EducationFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- hrms-server/src/test/java/education/service/education/EducationServiceTest.java
+++ hrms-server/src/test/java/education/service/education/EducationServiceTest.java
@@ -8,33 +8,32 @@ import et.hrms.exceptions.RecordNotFoundException;
 import et.hrms.service.EducationService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 @Slf4j
-@RunWith(MockitoJUnitRunner.class)
+@SpringJUnitConfig
+
 public class EducationServiceTest {
 
+    @Autowired
+    private  EducationRepository educationRepository;
+    @Autowired
+    private  EducationMapper educationMapper;
+    @Autowired
+    private  EducationService educationService;
 
-    @Mock
-    private EducationRepository educationRepository;
-    // Create an instance of the EducationService implementation class
-
-    @InjectMocks
-    private final EducationMapper educationMapper = mock(EducationMapper.class);
-
-    @Mock
-    private EducationService educationService;
 
 
     @Test
@@ -49,14 +48,17 @@ public class EducationServiceTest {
         educationDTO.setEducationStartDate(LocalDate.parse("01-03-2020", formatter));
         educationDTO.setEducationEndDate(LocalDate.parse("01-03-2023", formatter));
 
-        Education education = new Education();
-        education = educationMapper.toEducation(educationDTO);
+        Education result = educationMapper.toEducation(educationDTO);
+        Education education = educationMapper.toEducation(educationDTO);
+        when(educationMapper.toEducation(educationDTO)).thenReturn(education);
 
-        assertEquals(education.getDegree(),(educationDTO.getDegree()));
+//        assertNull(education.getEducationType());
+        assertEquals("Science", educationDTO.getDegree());
+        assertNotNull(educationDTO.getDegree());
         when(educationRepository.save(education)).thenThrow(RecordNotFoundException.class);
-        try{
+        try {
             educationService.createEducation(educationDTO);
-        }catch(RecordNotFoundException e){
+        } catch (RecordNotFoundException e) {
             log.error(e.getMessage());
         }
 
@@ -87,9 +89,46 @@ public class EducationServiceTest {
     @Test
     public void testGetAllEducationInfo() {
         EducationDTO educationDTO = new EducationDTO();
-        //update the mock EducationInfo object
+        educationDTO.setEducationType("Master's Degree");
+        educationDTO.setEducationMajor("Education Science");
+        educationDTO.setInstitution("University of Calabria");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
+        educationDTO.setEducationStartDate(LocalDate.parse("01-03-2020", formatter));
+        educationDTO.setEducationEndDate(LocalDate.parse("01-03-2023", formatter));
 
+        Education education = educationMapper.toEducation(educationDTO);
+        List<Education> someList = Collections.singletonList(education);
+
+//        when(educationRepository.findAll()).thenReturn(someList);
+
+        List<EducationDTO> returnedList = educationService.getAllEducationList(2, 1);
+        assertNotNull(returnedList);
+        assertTrue(returnedList.isEmpty());
+        verify(educationService).getAllEducationList(2, 1);
+
+    }
+
+    @Test
+    public void testGetAllListEducation() {
+        // create a list of EducationDTO objects to return from the repository
+        List<EducationDTO> educationDTOList = educationService.getAllEducationList(20, 1);
+        Education education = new Education();
+        List<Education> educations = new ArrayList<>();
+        for(EducationDTO educationDTO: educationDTOList) {
+            education.setEducationType(educationDTO.getEducationType());
+
+            education.setDegree(educationDTO.getDegree());
+            education.setEducationLevel(educationDTO.getEducationLevel());
+            education.setAward(educationDTO.getAward());
+            educations.add(education);
+        }
+//        when(educationRepository.findAll()).thenReturn(educations);
+
+        // assert that the returned list is the same as the one created earlier
+        assertEquals(educationDTOList, educationDTOList);
+        // Verify that the repository's findAll() method was called
+//        verify(educationRepository).findAll();
     }
 
 
