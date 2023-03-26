2f60bb2a28891875027bad6518374c475c741d01
GezahegnTsegaye <gezut38@gmail.com>
2023-03-26 12:22:20 +0200

    Modification GenderStatus and MaritalStatus enum files

--- hrms-server/src/main/java/et/hrms/dal/model/GenderStatus.java
+++ hrms-server/src/main/java/et/hrms/dal/model/GenderStatus.java
@@ -1,6 +1,47 @@
 package et.hrms.dal.model;
 
+/**
+ * Represents the gender status of an individual.
+ */
 public enum GenderStatus {
 
-    None, Male, Female, NonSpecific
+    /**
+     * Indicates no information is available for the gender status.
+     */
+    None("Not specified"),
+
+    /**
+     * Indicates the individual identifies as male.
+     */
+    Male("Male"),
+
+    /**
+     * Indicates the individual identifies as female.
+     */
+    Female("Female"),
+
+    /**
+     * Indicates the individual has a non-specific or non-binary gender identity.
+     */
+    NonSpecific("Non-specific");
+
+    private String displayName;
+
+    /**
+     * Constructs an instance of the enumeration with the given display name.
+     *
+     * @param displayName the human-readable display name for the enumeration value
+     */
+    GenderStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    /**
+     * Returns the human-readable display name for the enumeration value.
+     *
+     * @return the display name
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
 }
--- hrms-server/src/main/java/et/hrms/dal/model/MaritalStatus.java
+++ hrms-server/src/main/java/et/hrms/dal/model/MaritalStatus.java
@@ -1,6 +1,48 @@
 package et.hrms.dal.model;
 
 public enum MaritalStatus {
+    /**
+     * Indicates no information is available for the marital status.
+     */
+    None("Not specified"),
 
-    None,Single,Married, Divorced, Widowed
+    /**
+     * Indicates the individual is not married.
+     */
+    Single("Single"),
+
+    /**
+     * Indicates the individual is married.
+     */
+    Married("Married"),
+
+    /**
+     * Indicates the individual is divorced.
+     */
+    Divorced("Divorced"),
+
+    /**
+     * Indicates the individual's spouse has passed away.
+     */
+    Widowed("Widowed");
+
+    private String displayName;
+
+    /**
+     * Constructs an instance of the enumeration with the given display name.
+     *
+     * @param displayName the human-readable display name for the enumeration value
+     */
+    MaritalStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    /**
+     * Returns the human-readable display name for the enumeration value.
+     *
+     * @return the display name
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
 }
